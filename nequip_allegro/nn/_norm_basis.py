4782216de7adba76c54a67f29237ae44d569d427
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2022-02-22 16:08:39 -0500

    more basis normalization options

--- nequip_allegro/nn/_norm_basis.py
+++ nequip_allegro/nn/_norm_basis.py
@@ -18,24 +18,36 @@ class NormalizedBasis(torch.nn.Module):
 
     def __init__(
         self,
-        r_min: float,
         r_max: float,
+        r_min: float = 0.0,
         original_basis=BesselBasis,
         original_basis_kwargs: dict = {},
         n: int = 4000,
+        norm_basis_mean_shift: bool = True,
     ):
         super().__init__()
         self.basis = original_basis(**original_basis_kwargs)
         self.r_min = r_min
         self.r_max = r_max
+        assert self.r_min >= 0.0
+        assert self.r_max > r_min
         self.n = n
 
         self.num_basis = self.basis.num_basis
 
         # Uniform distribution on [r_min, r_max)
         with torch.no_grad():
-            rs = torch.linspace(r_min, r_max, n)
-            basis_std, basis_mean = torch.std_mean(self.basis(rs), dim=0)
+            # don't take 0 in case of weirdness like bessel at 0
+            rs = torch.linspace(r_min, r_max, n + 1)[1:]
+            bs = self.basis(rs)
+            assert bs.ndim == 2 and len(bs) == n
+            if norm_basis_mean_shift:
+                basis_std, basis_mean = torch.std_mean(bs, dim=0)
+            else:
+                basis_std = bs.square().mean().sqrt()
+                basis_mean = torch.as_tensor(
+                    0.0, device=basis_std.device, dtype=basis_std.dtype
+                )
 
         self.register_buffer("_mean", basis_mean)
         self.register_buffer("_inv_std", torch.reciprocal(basis_std))
