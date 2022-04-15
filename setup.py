9883c5f6b52ba09a3111f0f4febb9b78bb6240e0
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2022-04-15 17:19:38 -0400

    add versioning

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# see https://packaging.python.org/guides/single-sourcing-package-version/
+version_dict = {}
+with open(Path(__file__).parents[0] / "allegro/_version.py") as fp:
+    exec(fp.read(), version_dict)
+version = version_dict["__version__"]
+del version_dict
 
 setup(
     name="mir-allegro",
-    version="0.1.0",
+    version=version,
     author="Albert Musaelian, Simon Batzner",
     description="Allegro is an open-source code for building highly scalable and accurate equivariant deep learning interatomic potentials.",
     python_requires=">=3.7",
