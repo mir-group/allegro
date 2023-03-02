d110e68c779308c181f0e81786ab5cf9632d2501
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2023-03-01 23:00:05 -0500

    test norm_basis_mean_shift

--- tests/nn/test_norm_basis.py
+++ tests/nn/test_norm_basis.py
@@ -5,13 +5,23 @@ from allegro.nn import NormalizedBasis
 
 
 @pytest.mark.parametrize("r_min", [0.2, 1.0, 3.0])
-def test_normalized_basis(r_min):
+@pytest.mark.parametrize("norm_basis_mean_shift", [True, False])
+def test_normalized_basis(r_min, norm_basis_mean_shift):
     # Note that this parameter sharing is normally taken care of by `instantiate`
-    nb = NormalizedBasis(r_min=r_min, r_max=5.0, original_basis_kwargs={"r_max": 5.0})
+    nb = NormalizedBasis(
+        r_min=r_min,
+        r_max=5.0,
+        original_basis_kwargs={"r_max": 5.0, "num_bessels_per_basis": 8},
+        norm_basis_mean_shift=norm_basis_mean_shift,
+    )
     rs = torch.empty(10 * nb.n)
     rs.uniform_(nb.r_min, nb.r_max)
     bvals = nb(rs)
-    b_std, b_mean = torch.std_mean(bvals, dim=0)
     threshold = 2e-2  # pretty arbitrary, TODO
-    assert torch.all(b_mean.abs() < threshold)
-    assert torch.all((b_std - 1.0).abs() < threshold)
+    if norm_basis_mean_shift:
+        b_std, b_mean = torch.std_mean(bvals, dim=0)
+        assert torch.all(b_mean.abs() < threshold)
+        assert torch.all((b_std - 1.0).abs() < threshold)
+    else:
+        b_rms = bvals.square().mean(dim=0).sqrt()
+        assert torch.all((b_rms - 1.0).abs() < threshold)
