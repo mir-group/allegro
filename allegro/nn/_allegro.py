563faed604dd97224b231fe8bb72b28aec9d9631
Timothy J. Giese <TimothyJGiese@gmail.com>
2024-05-02 16:25:35 -0400

    allow calculation of isolated atom and well-separated dimer

--- allegro/nn/_allegro.py
+++ allegro/nn/_allegro.py
@@ -497,6 +497,12 @@ class Allegro_Module(GraphModuleMixin, torch.nn.Module):
             active_edges = (cutoff_coeffs > 0).nonzero().squeeze(-1)
 
             # Compute latents
+            for i in range(len(latent_inputs_to_cat)):
+                oldshape=latent_inputs_to_cat[i].shape
+                if len(latent_inputs_to_cat[i].shape)==1:
+                    latent_inputs_to_cat[i]=\
+                        latent_inputs_to_cat[i].reshape((oldshape[0],1))
+            
             new_latents = latent(torch.cat(latent_inputs_to_cat, dim=-1)[prev_mask])
             # Apply cutoff, which propagates through to everything else
             new_latents = cutoff_coeffs[active_edges].unsqueeze(-1) * new_latents
@@ -582,9 +588,14 @@ class Allegro_Module(GraphModuleMixin, torch.nn.Module):
             # Get invariants
             # features has shape [z][mul][k]
             # we know scalars are first
-            scalars = features[:, :, : self._n_scalar_outs[layer_index]].reshape(
-                features.shape[0], -1
-            )
+
+            oldshape = features[:, :, : self._n_scalar_outs[layer_index]].shape
+            newshape = (oldshape[0],oldshape[1]*oldshape[2])
+            scalars = features[:, :, : self._n_scalar_outs[layer_index]].reshape( newshape )
+            # The -1 breaks when there's an atom without an edge
+            # scalars = features[:, :, : self._n_scalar_outs[layer_index]].reshape(
+            #     features.shape[0], -1
+            # )
 
             # do the linear
             features = linear(features)
