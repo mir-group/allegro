15d5dbf572ac52e7be07d7329092eca95f083097
cw-tan <chuinwei.tan@gmail.com>
2024-11-12 00:25:54 -0500

    update allegro model tests with the new model builder

--- tests/model/test_allegro.py
+++ tests/model/test_allegro.py
@@ -1,53 +1,62 @@
 import pytest
 
-from nequip.data import AtomicDataDict
 from nequip.utils.unittests.model_tests import BaseEnergyModelTests
 
+BASIC_INFO = {
+    "_target_": "allegro.model.AllegroModel",
+    "seed": 123,
+    "type_names": ["H", "C", "O"],
+    "r_max": 4.0,
+}
+
 COMMON_CONFIG = {
+    "num_bessels": 4,
     "avg_num_neighbors": 5.0,  # very approximate to keep numerics sane
-    "r_max": 4,
-    "num_bessels_per_basis": 4,
-    "num_bases": 4,
-    "chemical_symbol_to_type": {"H": 0, "C": 1, "O": 2},
     "num_tensor_features": 4,
-    "two_body_latent_mlp_latent_dimensions": [32],
-    "latent_mlp_latent_dimensions": [32, 32],
-    "env_embed_mlp_latent_dimensions": [],
-    "edge_eng_mlp_latent_dimensions": [8],
-    # Just in case for when that builder exists:
-    "pair_style": "ZBL",
-    "units": "metal",
+    "two_body_latent_kwargs": {
+        "mlp_latent_dimensions": [32],
+    },
+    "latent_kwargs": {
+        "mlp_latent_dimensions": [32, 32],
+    },
+    "env_embed_kwargs": {
+        "mlp_latent_dimensions": [],
+    },
+    "edge_eng_kwargs": {
+        "mlp_latent_dimensions": [8],
+    },
+    **BASIC_INFO,
 }
 # TODO: test so3 mode when can pass down option to assert equivariance to ignore parity
 minimal_config1 = dict(
     l_max=1,
-    parity="o3_full",
+    parity_setting="o3_full",
     num_layers=1,
     **COMMON_CONFIG,
 )
 minimal_config2 = dict(
     l_max=3,
-    parity="o3_full",
+    parity_setting="o3_full",
     num_layers=2,
     per_edge_type_cutoff={"H": 2.0, "C": {"H": 4.0, "C": 3.5, "O": 3.7}, "O": 3.9},
     **COMMON_CONFIG,
 )
 minimal_config3 = dict(
     l_max=2,
-    parity="o3_restricted",
+    parity_setting="o3_restricted",
     num_layers=4,
     **COMMON_CONFIG,
 )
 minimal_config4 = dict(
     l_max=3,
-    parity="o3_full",
+    parity_setting="o3_full",
     num_layers=3,
     latent_resnet=False,
     **COMMON_CONFIG,
 )
 minimal_config5 = dict(
     l_max=3,
-    parity="o3_full",
+    parity_setting="o3_full",
     num_layers=3,
     latent_resnet=True,
     tensors_mixing_mode="uvvp",
@@ -55,7 +64,7 @@ minimal_config5 = dict(
 )
 minimal_config6 = dict(
     l_max=4,
-    parity="o3_full",
+    parity_setting="o3_full",
     num_layers=2,
     tensors_mixing_mode="p",
     **COMMON_CONFIG,
@@ -78,46 +87,7 @@ class TestAllegro(BaseEnergyModelTests):
         ],
         scope="class",
     )
-    def base_config(self, request):
-        return request.param
-
-    @pytest.fixture(
-        params=[
-            (
-                ["allegro.model.Allegro", "ForceOutput"],
-                [
-                    AtomicDataDict.TOTAL_ENERGY_KEY,
-                    AtomicDataDict.PER_ATOM_ENERGY_KEY,
-                    AtomicDataDict.FORCE_KEY,
-                    AtomicDataDict.EDGE_FEATURES_KEY,
-                ],
-            ),
-            (
-                ["allegro.model.Allegro", "StressForceOutput"],
-                [
-                    AtomicDataDict.TOTAL_ENERGY_KEY,
-                    AtomicDataDict.PER_ATOM_ENERGY_KEY,
-                    AtomicDataDict.FORCE_KEY,
-                    AtomicDataDict.STRESS_KEY,
-                    AtomicDataDict.VIRIAL_KEY,
-                ],
-            ),
-            (
-                ["allegro.model.Allegro", "PairPotentialTerm", "StressForceOutput"],
-                [
-                    AtomicDataDict.TOTAL_ENERGY_KEY,
-                    AtomicDataDict.PER_ATOM_ENERGY_KEY,
-                    AtomicDataDict.FORCE_KEY,
-                    AtomicDataDict.STRESS_KEY,
-                    AtomicDataDict.VIRIAL_KEY,
-                ],
-            ),
-        ],
-        scope="class",
-    )
-    def config(self, request, base_config):
-        config = base_config.copy()
-        builder, out_fields = request.param
+    def config(self, request):
+        config = request.param
         config = config.copy()
-        config["model_builders"] = builder
-        return config, out_fields
+        return config
