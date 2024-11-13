45c634d743e1388c5d503bb6c4a5e531382d543a
cw-tan <chuinwei.tan@gmail.com>
2024-11-13 00:39:50 -0500

    revamp `ProductTypeEmbedding`

--- allegro/nn/_edgeembed.py
+++ allegro/nn/_edgeembed.py
@@ -4,7 +4,7 @@ from e3nn import o3
 from e3nn.util.jit import compile_mode
 
 from nequip.data import AtomicDataDict
-from nequip.nn import GraphModuleMixin, with_edge_vectors_
+from nequip.nn import GraphModuleMixin
 
 from ._fc import ScalarMLPFunction
 
@@ -16,8 +16,8 @@ class ProductTypeEmbedding(GraphModuleMixin, torch.nn.Module):
     """Take a radial edge embedding and combine it with type embedding information through an elementwise product in an embedding space.
 
     Args:
-        initial_scalar_embedding_dim (int): the dimension of the embedding space
-        basis_mlp_kwargs: options for the MLP embedding the previous radial basis. By default no hidden layers or nonlinearities (linear projection).
+        type_names (List[str]): list of atom type names
+        initial_embedding_dim (int): the dimension of the initial embedding space
     """
 
     num_types: int
@@ -25,61 +25,63 @@ class ProductTypeEmbedding(GraphModuleMixin, torch.nn.Module):
     def __init__(
         self,
         type_names: List[str],
-        initial_scalar_embedding_dim: int,
-        radial_basis_mlp=ScalarMLPFunction,
-        radial_basis_mlp_kwargs={},
+        initial_embedding_dim: int,
+        # bookkeeping
+        edge_type_field: str = AtomicDataDict.EDGE_TYPE_KEY,
+        radial_features_in_field=AtomicDataDict.EDGE_EMBEDDING_KEY,
+        edge_embed_out_field=AtomicDataDict.EDGE_EMBEDDING_KEY,
         irreps_in=None,
     ):
         super().__init__()
         self._init_irreps(irreps_in=irreps_in)
+
+        # bookkeeping
+        self.edge_type_field = edge_type_field
+        self.in_field = radial_features_in_field
+        self.out_field = edge_embed_out_field
         self.num_types = len(type_names)
-        assert initial_scalar_embedding_dim % 2 == 0
-        self.type_embeddings = torch.nn.Parameter(
-            torch.randn(2, self.num_types, initial_scalar_embedding_dim // 2)
+
+        # == type embedding ==
+        assert (
+            initial_embedding_dim % 2 == 0
+        ), "`initial_embedding_dim` must be an even number"
+
+        self.center_embed = torch.nn.Embedding(
+            num_embeddings=self.num_types,
+            embedding_dim=initial_embedding_dim // 2,
+        )
+        self.neighbor_embed = torch.nn.Embedding(
+            num_embeddings=self.num_types,
+            embedding_dim=initial_embedding_dim // 2,
         )
 
-        # default
-        opts = dict(mlp_latent_dimensions=[], mlp_nonlinearity=None)
-        opts.update(radial_basis_mlp_kwargs)
-        self.basis_mlp = radial_basis_mlp(
-            mlp_input_dimension=self.irreps_in[
-                AtomicDataDict.EDGE_EMBEDDING_KEY
-            ].num_irreps,  # TODO check scalar
-            mlp_output_dimension=initial_scalar_embedding_dim,
-            **opts,
+        # == radial basis linear projection ==
+        self.basis_linear = ScalarMLPFunction(
+            mlp_input_dim=self.irreps_in[self.in_field].num_irreps,
+            mlp_hidden_layer_dims=[],
+            mlp_output_dim=initial_embedding_dim,
+            mlp_nonlinearity=None,
+            mlp_bias=None,
         )
+        assert not self.basis_linear.is_nonlinear
 
-        self.irreps_out[AtomicDataDict.EDGE_EMBEDDING_KEY] = o3.Irreps(
-            [(initial_scalar_embedding_dim, (0, 1))]
-        )
+        self.irreps_out[self.out_field] = o3.Irreps([(initial_embedding_dim, (0, 1))])
 
     def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
-        data = with_edge_vectors_(data, with_lengths=True)
-        # embed types
-        atom_types = data[AtomicDataDict.ATOM_TYPE_KEY].squeeze(-1)
-        edge_types = torch.index_select(
-            atom_types, 0, data[AtomicDataDict.EDGE_INDEX_KEY].reshape(-1)
-        ).view(2, -1)
-        center_types = edge_types[0]
-        neighbor_types = edge_types[1]
-        center_embed = torch.index_select(
-            self.type_embeddings[0],
-            0,
-            center_types,
-        )
-        neighbor_embed = torch.index_select(
-            self.type_embeddings[1],
-            0,
-            neighbor_types,
-        )
-
-        # project basis out to type embedding dimension
-        basis = self.basis_mlp(data[AtomicDataDict.EDGE_EMBEDDING_KEY])
+        # == embed atom types ==
+        if self.edge_type_field in data:
+            edge_types = data[self.edge_type_field]
+        else:
+            edge_types = torch.index_select(
+                data[AtomicDataDict.ATOM_TYPE_KEY].reshape(-1),
+                0,
+                data[AtomicDataDict.EDGE_INDEX_KEY].reshape(-1),
+            ).view(2, -1)
         type_embed = torch.cat(
-            (center_embed, neighbor_embed),
+            (self.center_embed(edge_types[0]), self.neighbor_embed(edge_types[1])),
             dim=-1,
         )
-        edge_embed = type_embed * basis
-
-        data[AtomicDataDict.EDGE_EMBEDDING_KEY] = edge_embed
+        # project radial basis out to type embedding dimension and multiply
+        basis = self.basis_linear(data[self.in_field])
+        data[self.out_field] = type_embed * basis
         return data
