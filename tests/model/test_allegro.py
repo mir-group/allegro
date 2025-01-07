c29f9f0ba5347a7e427586a6e496a88ce30aaf8a
cw-tan <chuinwei.tan@gmail.com>
2025-01-07 15:51:57 -0500

    revamp allegro model tests

--- tests/model/test_allegro.py
+++ tests/model/test_allegro.py
@@ -1,13 +1,34 @@
 import pytest
-
 from nequip.utils.unittests.model_tests import BaseEnergyModelTests
 
-BASIC_INFO = {
+
+COMMON_CONFIG = {
     "_target_": "allegro.model.AllegroModel",
     "seed": 123,
     "type_names": ["H", "C", "O"],
     "r_max": 4.0,
+    "avg_num_neighbors": 5.0,  # very approximate to keep numerics sane
+    "num_layers": 2,
+    "l_max": 2,
+    "num_scalar_features": 32,
+    "num_tensor_features": 4,
+    "allegro_mlp_hidden_layer_depth": 2,
+    "allegro_mlp_hidden_layer_width": 32,
+    "readout_mlp_hidden_layer_depth": 1,
+    "readout_mlp_hidden_layer_width": 8,
 }
+
+minimal_config0 = dict(
+    **COMMON_CONFIG,
+)
+minimal_config1 = dict(
+    per_edge_type_cutoff={"H": 2.0, "C": {"H": 4.0, "C": 3.5, "O": 3.7}, "O": 3.9},
+    **COMMON_CONFIG,
+)
+minimal_config2 = dict(
+    tensors_mixing_mode="uvvp",
+    **COMMON_CONFIG,
+)
 
 BESSEL_CONFIG = {
     "_target_": "allegro.nn.TwoBodyBesselScalarEmbed",
@@ -23,57 +44,6 @@ SPLINE_CONFIG = {
     "spline_span": 3,
 }
 
-COMMON_CONFIG = {
-    "avg_num_neighbors": 5.0,  # very approximate to keep numerics sane
-    "num_scalar_features": 32,
-    "num_tensor_features": 4,
-    "allegro_mlp_hidden_layer_depth": 2,
-    "allegro_mlp_hidden_layer_width": 32,
-    "readout_mlp_hidden_layer_depth": 1,
-    "readout_mlp_hidden_layer_width": 8,
-    **BASIC_INFO,
-}
-# TODO: test so3 mode when can pass down option to assert equivariance to ignore parity
-minimal_config1 = dict(
-    l_max=1,
-    parity_setting="o3_full",
-    num_layers=1,
-    **COMMON_CONFIG,
-)
-minimal_config2 = dict(
-    l_max=3,
-    parity_setting="o3_full",
-    num_layers=2,
-    per_edge_type_cutoff={"H": 2.0, "C": {"H": 4.0, "C": 3.5, "O": 3.7}, "O": 3.9},
-    **COMMON_CONFIG,
-)
-minimal_config3 = dict(
-    l_max=2,
-    parity_setting="o3_restricted",
-    num_layers=2,
-    **COMMON_CONFIG,
-)
-minimal_config4 = dict(
-    l_max=3,
-    parity_setting="o3_full",
-    num_layers=3,
-    **COMMON_CONFIG,
-)
-minimal_config5 = dict(
-    l_max=2,
-    parity_setting="o3_full",
-    num_layers=3,
-    tensors_mixing_mode="uvvp",
-    **COMMON_CONFIG,
-)
-minimal_config6 = dict(
-    l_max=4,
-    parity_setting="o3_full",
-    num_layers=2,
-    tensors_mixing_mode="p",
-    **COMMON_CONFIG,
-)
-
 
 class TestAllegro(BaseEnergyModelTests):
     @pytest.fixture
@@ -87,6 +57,14 @@ class TestAllegro(BaseEnergyModelTests):
     def scalar_embed_config(self, request):
         return request.param
 
+    # TODO: test so3 mode when can pass down option to assert equivariance to ignore parity
+    @pytest.fixture(
+        params=["o3_full", "o3_restricted"],
+        scope="class",
+    )
+    def parity_setting(self, request):
+        return request.param
+
     @pytest.fixture(
         params=[True, False],
         scope="class",
@@ -95,19 +73,32 @@ class TestAllegro(BaseEnergyModelTests):
         return request.param
 
     @pytest.fixture(
+        params=[True, False],
+        scope="class",
+    )
+    def node_readout(self, request):
+        return request.param
+
+    @pytest.fixture(
         params=[
+            minimal_config0,
             minimal_config1,
-            minimal_config2,
-            minimal_config3,
-            minimal_config4,
-            minimal_config5,
-            minimal_config6,
+            # minimal_config2,
         ],
         scope="class",
     )
-    def config(self, request, scalar_embed_config, scatter_features):
+    def config(
+        self,
+        request,
+        scalar_embed_config,
+        parity_setting,
+        scatter_features,
+        node_readout,
+    ):
         config = request.param
         config = config.copy()
         config.update({"scalar_embed": scalar_embed_config})
+        config.update({"parity_setting": parity_setting})
         config.update({"scatter_features": scatter_features})
+        config.update({"node_readout": node_readout})
         return config
