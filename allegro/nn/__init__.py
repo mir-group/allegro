c4fb869a5b31b3b93bd33808a0465a77bf43dce4
cw-tan <chuinwei.tan@gmail.com>
2025-01-23 00:13:30 -0500

    remove MLP module and test (moved to `nequip`)

--- allegro/nn/__init__.py
+++ allegro/nn/__init__.py
@@ -1,6 +1,5 @@
 from ._allegro import Allegro_Module
 from .edgewise import EdgewiseReduce
-from ._fc import ScalarMLP, ScalarMLPFunction
 from ._edgeembed import ProductTypeEmbedding
 from .scalarembed import TwoBodyBesselScalarEmbed, TwoBodySplineScalarEmbed
 from .tensorembed import TwoBodySphericalHarmonicTensorEmbed
@@ -9,8 +8,6 @@ from ._misc import ScalarMultiply
 __all__ = [
     Allegro_Module,
     EdgewiseReduce,
-    ScalarMLP,
-    ScalarMLPFunction,
     ProductTypeEmbedding,
     TwoBodyBesselScalarEmbed,
     TwoBodySplineScalarEmbed,
