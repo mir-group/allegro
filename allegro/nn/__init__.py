8935a8cef2825c69b1a85dcdd3d44de1392241c6
cw-tan <chuinwei.tan@gmail.com>
2025-06-03 23:43:42 -0400

    remove unused `nn/_misc.py`

--- allegro/nn/__init__.py
+++ allegro/nn/__init__.py
@@ -4,7 +4,6 @@ from .edgewise import EdgewiseReduce
 from ._edgeembed import ProductTypeEmbedding
 from .scalarembed import TwoBodyBesselScalarEmbed, TwoBodySplineScalarEmbed
 from .tensorembed import TwoBodySphericalHarmonicTensorEmbed
-from ._misc import ScalarMultiply
 
 __all__ = [
     Allegro_Module,
@@ -13,5 +12,4 @@ __all__ = [
     TwoBodyBesselScalarEmbed,
     TwoBodySplineScalarEmbed,
     TwoBodySphericalHarmonicTensorEmbed,
-    ScalarMultiply,
 ]
